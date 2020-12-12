08ed989e4661b5d3a939dbaa0f901b452a06cc6d
Luis <Luis@DESKTOP-1IS3NNC>
2020-12-12 17:50:57 -0300

    Actualizado frontend con nuevas interfaces

---
+++ ControlCalidad.Cliente.Presentacion/Interfaces/IVistaInteractiva.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlCalidad.Cliente.Presentacion.Interfaces
+{
+    public interface IVistaInteractiva
+    {
+        void Cerrar();
+        void MostrarMensaje(string mensaje, bool esError = false);
+    }
+}
